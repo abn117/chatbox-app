467639d2fc9bf4f9ae2aaf4e97dd26d48c62ba02
abn117 <antoine.bohssain@gmail.com>
2019-01-09 16:04:16 +0100

    done without pretty css

---
+++ src/components/Connexion.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
+import PropTypes from 'prop-types';
+
+class Connexion extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            pseudo: '',
+            goToChat: false
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const pseudo = event.target.value;
+        this.setState({ pseudo: pseudo })
+    
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.setState({ goToChat: true });
+    }
+
+    render() {
+        if (this.state.goToChat) {
+            return <Redirect to={`/pseudo/${this.state.pseudo}`}/>
+        }
+        return (
+            <div className="container">
+                <form className="" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <input
+                            value={this.state.pseudo}
+                            onChange={this.handleChange}
+                            placeholder="Saisir un pseudo"
+                            type="text"
+                            required
+                        />
+                        <button type="submit">Connexion</button>
+                    </div>
+                </form>
+            </div>
+        );
+    };
+
+}
+
+Connexion.propTypes = {
+    
+};
+
+export default Connexion;
---
+++ src/components/NotFound.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class NotFound extends Component {
+    render(){
+        return (
+            <h1>Sorry this page is not found :/</h1>
+        )
+    }
+}
+
+NotFound.propTypes = {
+    
+};
+
+export default NotFound;
